4b4b1de2ec829576293cd0191e4bdded85ad50d9
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-04-07 22:00:00 +1000

    refactor: replace getState() switch with object

--- js/script.js
+++ js/script.js
@@ -156,70 +156,68 @@ class App {
     }
 
     getState(member) {
-        const currentTerm = member.terms.at(-1);
-        switch (currentTerm.state) {
-            case "AL": return "Alabama";
-            case "AK": return "Alaska";
-            case "AZ": return "Arizona";
-            case "AR": return "Arkansas";
-            case "CA": return "California";
-            case "CO": return "Colorado";
-            case "CT": return "Connecticut";
-            case "DE": return "Delaware";
-            case "FL": return "Florida";
-            case "GA": return "Georgia";
-            case "HI": return "Hawaii";
-            case "ID": return "Idaho";
-            case "IL": return "Illinois";
-            case "IN": return "Indiana";
-            case "IA": return "Iowa";
-            case "KS": return "Kansas";
-            case "KY": return "Kentucky";
-            case "LA": return "Louisiana";
-            case "ME": return "Maine";
-            case "MD": return "Maryland";
-            case "MA": return "Massachusetts";
-            case "MI": return "Michigan";
-            case "MN": return "Minnesota";
-            case "MS": return "Mississippi";
-            case "MO": return "Missouri";
-            case "MT": return "Montana";
-            case "NE": return "Nebraska";
-            case "NV": return "Nevada";
-            case "NH": return "New Hampshire";
-            case "NJ": return "New Jersey";
-            case "NM": return "New Mexico";
-            case "NY": return "New York";
-            case "NC": return "North Carolina";
-            case "ND": return "North Dakota";
-            case "OH": return "Ohio";
-            case "OK": return "Oklahoma";
-            case "OR": return "Oregon";
-            case "PA": return "Pennsylvania";
-            case "RI": return "Rhode Island";
-            case "SC": return "South Carolina";
-            case "SD": return "South Dakota";
-            case "TN": return "Tennessee";
-            case "TX": return "Texas";
-            case "UT": return "Utah";
-            case "VT": return "Vermont";
-            case "VA": return "Virginia";
-            case "WA": return "Washington";
-            case "WV": return "West Virginia";
-            case "WI": return "Wisconsin";
-            case "WY": return "Wyoming";
+        const fullname = {
+            AL: "Alabama",
+            AK: "Alaska",
+            AZ: "Arizona",
+            AR: "Arkansas",
+            CA: "California",
+            CO: "Colorado",
+            CT: "Connecticut",
+            DE: "Delaware",
+            FL: "Florida",
+            GA: "Georgia",
+            HI: "Hawaii",
+            ID: "Idaho",
+            IL: "Illinois",
+            IN: "Indiana",
+            IA: "Iowa",
+            KS: "Kansas",
+            KY: "Kentucky",
+            LA: "Louisiana",
+            ME: "Maine",
+            MD: "Maryland",
+            MA: "Massachusetts",
+            MI: "Michigan",
+            MN: "Minnesota",
+            MS: "Mississippi",
+            MO: "Missouri",
+            MT: "Montana",
+            NE: "Nebraska",
+            NV: "Nevada",
+            NH: "New Hampshire",
+            NJ: "New Jersey",
+            NM: "New Mexico",
+            NY: "New York",
+            NC: "North Carolina",
+            ND: "North Dakota",
+            OH: "Ohio",
+            OK: "Oklahoma",
+            OR: "Oregon",
+            PA: "Pennsylvania",
+            RI: "Rhode Island",
+            SC: "South Carolina",
+            SD: "South Dakota",
+            TN: "Tennessee",
+            TX: "Texas",
+            UT: "Utah",
+            VT: "Vermont",
+            VA: "Virginia",
+            WA: "Washington",
+            WV: "West Virginia",
+            WI: "Wisconsin",
+            WY: "Wyoming",
 
             // Territories
-            case "DC": return "District of Columbia";
-            case "PR": return "Puerto Rico";
-            case "GU": return "Guam";
-            case "VI": return "U.S. Virgin Islands";
-            case "AS": return "American Samoa";
-            case "MP": return "Northern Mariana Islands";
-
-            default:
-                return "Invalid";
-        }
+            DC: "District of Columbia",
+            PR: "Puerto Rico",
+            GU: "Guam",
+            VI: "U.S. Virgin Islands",
+            AS: "American Samoa",
+            MP: "Northern Mariana Islands"
+        };
+        const currentTerm = member.terms.at(-1);
+        return fullname[currentTerm.state];
     }
 
     getParty(member) {
